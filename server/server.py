66c6d19515fec59916ccf816cddd8fdc78cd620f
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-01 00:08:59 +0300

    Add more error checks to server

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 import aiofiles
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -21,41 +22,63 @@ class Item(BaseModel):
     isComplete: bool
 
 
+async def open_file() -> List[Item]:
+    try:
+        async with aiofiles.open("db.json", mode="r") as db:
+            raw_data = await db.read()
+    except FileNotFoundError:
+        return []
+    if not raw_data:
+        return []
+    return json.loads(raw_data)
+
+
+async def write_file(data: List[Item]) -> bool:
+    raw_data = json.dumps(data)
+    async with aiofiles.open("db.json", mode="w") as db:
+        result = await db.write(raw_data)
+    return result == len(raw_data)
+
+
 @app.get("/api/items")
 async def read_items():
-    async with aiofiles.open("db.json", mode="r") as db:
-        raw_content = await db.read()
-    if not raw_content:
-        return []
-    return json.loads(raw_content)
+    return await open_file()
 
 
 @app.post("/api/items")
 async def create_item(item: Item):
-    async with aiofiles.open("db.json", mode="r") as db:
-        raw_content = await db.read()
-    data = json.loads(raw_content)
+    data = await open_file()
     data.append(item.dict())
-    async with aiofiles.open("db.json", mode="w") as db:
-        await db.write(json.dumps(data))
+    if not await write_file(data):
+        raise HTTPException(
+            status_code=400, detail="Error happened while writing items to file"
+        )
     return item
 
 
 @app.put("/api/items/{item_id}")
 async def replace_item(item_id: str, item: Item):
-    async with aiofiles.open("db.json", mode="r") as db:
-        raw_content = await db.read()
-    data = json.loads(raw_content)
+    data = await open_file()
+    if not any(x["id"] == item_id for x in data):
+        raise HTTPException(status_code=404, detail="Not found")
+
     data = [item.dict() if x["id"] == item_id else x for x in data]
-    async with aiofiles.open("db.json", mode="w") as db:
-        await db.write(json.dumps(data))
+    if not await write_file(data):
+        raise HTTPException(
+            status_code=400, detail="Error happened while writing items to file"
+        )
+    return item
 
 
 @app.delete("/api/items/{item_id}")
 async def delete_item(item_id: str):
-    async with aiofiles.open("db.json", mode="r") as db:
-        raw_content = await db.read()
-    data = json.loads(raw_content)
+    data = await open_file()
+    if not any(x["id"] == item_id for x in data):
+        raise HTTPException(status_code=404, detail="Not found")
+
     data = list(filter(lambda x: x["id"] == item_id, data))
-    async with aiofiles.open("db.json", mode="w") as db:
-        await db.write(json.dumps(data))
+    if not await write_file(data):
+        raise HTTPException(
+            status_code=400, detail="Error happened while writing items to file"
+        )
+    return item
