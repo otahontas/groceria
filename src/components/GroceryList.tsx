24ef18c50de65352a0597b0bfa582eed9c7a68a7
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-08-31 18:07:10 +0300

    Add grocery list component

---
+++ src/components/GroceryList.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+
+import { CircularProgress, Container, Divider, Fab, List, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
+import Flip from "react-tiny-flip";
+import { useRecoilState, useSetRecoilState } from "recoil";
+
+import grocecyListService from "../services/grocecyListService";
+import { groceryListState, snackBarMessageState } from "../state/atoms";
+import { Item } from "../types";
+
+import { GroceryListItem } from "./GroceryListItem";
+import { GroceryListItemCreator } from "./GroceryListItemCreator";
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    position: "relative",
+    zIndex: 2,
+    display: "flex",
+    justifyContent: "flex-end",
+    transform: "translateY(-50%)",
+    marginBottom: `-${theme.spacing(4)}px`,
+  },
+  infoText: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export const GroceryList = () => {
+  const [groceryList, setGroceryList] = useRecoilState(groceryListState);
+  const setSnackbarMessage = useSetRecoilState(snackBarMessageState);
+
+  const [showGroceryListItemCreator, setShowGroceryListItemCreator] = useState<boolean>(false);
+  const [fetchStatus, setFetchStatus] = useState<"unloaded" | "loading" | "loaded">("unloaded");
+
+  const classes = useStyles();
+  const toggleAddForm = () => setShowGroceryListItemCreator(showAddForm => !showAddForm);
+  useEffect(() => {
+    setFetchStatus("loading");
+    const fetchList = async () => {
+      const response = await grocecyListService.getAll();
+      if (response.ok && response.data) {
+        setGroceryList(response.data);
+        setFetchStatus("loaded");
+      } else {
+        setFetchStatus("unloaded");
+        setSnackbarMessage("Couldn't load items, please try again");
+      }
+    };
+    fetchList();
+  }, [setGroceryList, setFetchStatus]);
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Paper>
+        <div className={classes.fab}>
+          <Fab aria-label="add" onClick={toggleAddForm}>
+            {showGroceryListItemCreator ? <CloseIcon /> : <AddIcon />}
+          </Fab>
+        </div>
+        {showGroceryListItemCreator ? <GroceryListItemCreator toggle={toggleAddForm} /> : null}
+        {fetchStatus === "unloaded" && null}
+        {fetchStatus === "loading" && <CircularProgress color="secondary" />}
+        {fetchStatus === "loaded" && groceryList.length === 0 ? (
+          <Container maxWidth="xs" className={classes.infoText}>
+            <Typography align="center">Use the plus icon to add a new list item.</Typography>
+          </Container>
+        ) : (
+          <List>
+            <Flip>
+              {groceryList.map((item: Item) => (
+                <div key={item.id}>
+                  <GroceryListItem item={item} />
+                  {item !== groceryList[groceryList.length - 1] && <Divider />}
+                </div>
+              ))}
+            </Flip>
+          </List>
+        )}
+      </Paper>
+    </Container>
+  );
+};
