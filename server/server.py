6f92140e98b6d1dde25e5d6453988ef94d7bc842
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-02 08:48:47 +0300

    Remove cors from backend

--- server/server.py
+++ server/server.py
@@ -1,23 +1,10 @@
 import aiofiles
 import json
 from fastapi import FastAPI, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
-
-origins = [
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 class Item(BaseModel):
